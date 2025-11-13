7f06256baac1a9dda788517ad2269d3bb71bd40e
Surmachev <sorm1k@mail.ru>
2025-11-13 11:48:27 +0300

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Изменённая версия приложения на Render!"
+    return "Изменённая версия пна Render!"
 
 if __name__ == "__main__":
     app.run()
